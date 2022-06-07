7670f11902aedf6de389f8e8dabff8d7e0534d9e
krasnovss91 <krasnovstanislav91@gmail.com>
2022-06-07 18:19:27 +0400

    testMethods

--- src/test/kotlin/SampleTest.kt
+++ src/test/kotlin/SampleTest.kt
@@ -9,4 +9,22 @@ internal class SampleTest {
         val expected = 42
         assertEquals(expected,testSample.sum(40,2))
     }
+
+    @Test
+    fun testSubstract(){
+        val expected = 13
+        assertEquals(expected,testSample.substract(20,7))
+    }
+
+    @Test
+    fun testMul(){
+        val expected = 56
+        assertEquals(expected,testSample.mul(7,8))
+    }
+
+    @Test
+    fun testDiv(){
+        val expected = 3
+        assertEquals(expected,testSample.div(15,5))
+    }
 }
