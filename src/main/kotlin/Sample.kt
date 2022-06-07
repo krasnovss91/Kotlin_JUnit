373cead6edcc9fdf830c01362e332a6e05270a33
krasnovss91 <krasnovstanislav91@gmail.com>
2022-06-07 18:13:46 +0400

    methods

--- src/main/kotlin/Sample.kt
+++ src/main/kotlin/Sample.kt
@@ -3,4 +3,16 @@ class Sample {
     fun sum(a: Int, b:Int):Int{
         return a + b
     }
+
+    fun substract(a:Int, b:Int):Int{
+        return a - b
+    }
+
+    fun mul(a: Int, b: Int):Int{
+        return a * b
+    }
+
+    fun div(a: Int, b: Int):Int{
+        return a / b
+    }
 }
